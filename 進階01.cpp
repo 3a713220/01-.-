a8efa02422bd4200f920425acb8e06bb3ed0acdc
3a713220 <48341322+3a713220@users.noreply.github.com>
2019-04-19 01:19:04 +0800

    Add files via upload

---
+++ 進階01.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	char l;
+	for (l=65; l<=90; l++)
+	
+	{
+		printf("%c ",l);
+	}
+	
+	printf("\n");
+	for (l=97; l<=122; l++)
+	
+	{
+		printf("%c ",l);
+	}
+	
+	printf("\n");
+	system ("pause");
+	return 0;
+}
